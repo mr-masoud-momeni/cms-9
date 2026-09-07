352bfe2c87d684b87aae1a50bada6a3cea53e8e2
masoud <56384888+mr-masoud-momeni@users.noreply.github.com>
2026-09-08 01:12:52 +0330

    Use resolved public shop context

--- app/Helpers/ShopHelper.php
+++ app/Helpers/ShopHelper.php
@@ -7,19 +7,18 @@ use App\Models\Shop;
 
 class ShopHelper
 {
-
-    // گرفتن مدل فروشگاه از دامنه
     public static function getShop()
     {
-        $host = request()->getHost();
-        return Shop::where('domain', $host)->first(); // orFail نذار که 500 نده
+        if (request()->attributes->has('current_shop')) {
+            return request()->attributes->get('current_shop');
+        }
 
+        return app(Shop::class);
     }
 
-    // گرفتن فقط id فروشگاه
     public static function getShopId()
     {
         $shop = self::getShop();
-        return $shop ? $shop->id : null;
+        return $shop?->id;
     }
 }
