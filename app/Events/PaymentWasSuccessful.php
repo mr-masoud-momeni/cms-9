abe6b18492612bb92322b4d6a2249ffb32b60464
masoud <56384888+mr-masoud-momeni@users.noreply.github.com>
2026-09-07 10:15:51 +0330

    Fix successful payment event payload

--- app/Events/PaymentWasSuccessful.php
+++ app/Events/PaymentWasSuccessful.php
@@ -2,37 +2,18 @@
 
 namespace App\Events;
 
-use Illuminate\Broadcasting\Channel;
-use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use App\Models\Order;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
 class PaymentWasSuccessful
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
+    use Dispatchable, SerializesModels;
 
-    /**
-     * Create a new event instance.
-     *
-     * @return void
-     */
     public $payment;
 
-    public function __construct(Payment $order)
+    public function __construct(Order $order)
     {
-        $this->order = $order;
-    }
-
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return \Illuminate\Broadcasting\Channel|array
-     */
-    public function broadcastOn()
-    {
-        return new PrivateChannel('channel-name');
+        $this->payment = $order->payment;
     }
 }
