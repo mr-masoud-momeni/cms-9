13b3cb23c7cadf2de6d48442db3ae5171cbbe16d
masoud <56384888+mr-masoud-momeni@users.noreply.github.com>
2026-09-08 01:13:57 +0330

    Restore original throttle middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,7 +40,7 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
-        'throttle' => \Illuminate\Foundation\Http\Middleware\ThrottleRequests::class,
+        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'auth.buyer' => \App\Http\Middleware\AuthBuyer::class,
         'buyer.verified' => \App\Http\Middleware\BuyerVerified::class,
