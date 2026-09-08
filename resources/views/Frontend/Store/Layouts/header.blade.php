ea6cd93f6794a6375fb60105452fba2b53ce13db
masoud <56384888+mr-masoud-momeni@users.noreply.github.com>
2026-09-08 11:19:55 +0330

    Load shared Store theme styles

--- resources/views/Frontend/Store/Layouts/header.blade.php
+++ resources/views/Frontend/Store/Layouts/header.blade.php
@@ -6,6 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href="{{ asset('/frontend/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
     <link href="{{ asset('/frontend/vendor/custom.css') }}" rel="stylesheet">
+    <link href="{{ asset('/frontend/vendor/store-theme.css') }}" rel="stylesheet">
 </head>
 <body>
 <header class="store-header">
