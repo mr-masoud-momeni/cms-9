701ebae3d9e5c934552a3968028aa5b06e8d709b
masoud <56384888+mr-masoud-momeni@users.noreply.github.com>
2026-09-08 13:34:17 +0330

    Polish Store article detail view

--- resources/views/Frontend/Store/Pages/article.blade.php
+++ resources/views/Frontend/Store/Pages/article.blade.php
@@ -5,11 +5,14 @@
         <div class="store-detail-grid">
             <article class="store-detail-content">
                 <h1>{{ $article->title }}</h1>
-                <div class="store-blog-content">{!! $article->body !!}</div>
 
-                @if($article->categories->count())
+                <div class="store-blog-content">
+                    {!! $article->body !!}
+                </div>
+
+                @if(isset($article->category))
                     <div class="store-blog-meta">
-                        @foreach($article->categories as $cat)
+                        @foreach($article->category()->get() as $cat)
                             <span>{{ $cat->name }}</span>
                         @endforeach
                     </div>
@@ -20,7 +23,7 @@
                 </div>
             </article>
 
-            <div>
+            <div class="store-detail-media">
                 @if(!empty($article->images['thum']))
                     <img src="{{ asset($article->images['thum']) }}" alt="{{ $article->title }}" class="store-blog-image">
                 @endif
