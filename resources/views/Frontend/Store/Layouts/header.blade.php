304f79b884159e3e7347d6fdfdaa9569c3622cc9
masoud <56384888+mr-masoud-momeni@users.noreply.github.com>
2026-09-08 08:48:47 +0330

    Create independent storefront header

---
+++ resources/views/Frontend/Store/Layouts/header.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- Favicons -->
+    <link href="assets/img/favicon.png" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Montserrat:300,400,500,600,700" rel="stylesheet">
+    <link href="{{asset('/frontend/vendor/aos/aos.css')}}" rel="stylesheet">
+    <link href="{{asset('/frontend/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('/frontend/vendor/bootstrap/css/bootstrap-rtl.css')}}" rel="stylesheet">
+    <link href="{{asset('/frontend/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
+    <link href="{{asset('/frontend/vendor/glightbox/css/glightbox.min.css')}}" rel="stylesheet">
+    <link href="{{asset('/frontend/vendor/swiper/swiper-bundle.min.css')}}" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css"/>
+    <link href="{{asset('/frontend/vendor/custom.css')}}" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.5.0.js"></script>
+    <link href="{{asset('/frontend/css/style.css')}}" rel="stylesheet">
+</head>
+
+<body>
+<header id="header" class="fixed-top d-flex align-items-center header-transparent">
+    <div class="container d-flex align-items-center">
+
+        <h1 class="logo logo-align"><a href="index.html">Rapid</a></h1>
+
+        <nav id="navbar" class="navbar order-last order-lg-0">
+            <ul>
+                @php
+                    if(isset($menu->content)){
+                        $arrays = json_decode($menu->content , TRUE);
+                    }
+                @endphp
+                @if(isset($arrays))
+                    {{menu_navigation($arrays)}}
+                @endif
+
+            </ul>
+            <i class="bi bi-list mobile-nav-toggle"></i>
+        </nav>
+        <div class="social-links">
+            <span class="badge badge-success">Success</span><a href="{{route('buyer.order.index')}}" class="shop-ico"><i class="bi bi-cart-fill" id="cart-val" value={{$orderNumber}}></i></a>
+            <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+            <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+            <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+            <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+        </div>
+
+    </div>
+</header>
