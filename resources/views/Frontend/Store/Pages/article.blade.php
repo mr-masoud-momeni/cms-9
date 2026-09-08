9d778387495117c72f05bdc44034eae50bdbfa46
masoud <56384888+mr-masoud-momeni@users.noreply.github.com>
2026-09-08 11:00:22 +0330

    Move store article view into Store theme

---
+++ resources/views/Frontend/Store/Pages/article.blade.php
@@ -0,0 +1,30 @@
+@extends('Frontend.Store.Layouts.MasterMinimal')
+
+@section('Main')
+    <main class="store-detail store-blog-detail" aria-label="مقاله">
+        <div class="store-detail-grid">
+            <article class="store-detail-content">
+                <h1>{{ $article->title }}</h1>
+                <div class="store-blog-content">{!! $article->body !!}</div>
+
+                @if($article->categories->count())
+                    <div class="store-blog-meta">
+                        @foreach($article->categories as $cat)
+                            <span>{{ $cat->name }}</span>
+                        @endforeach
+                    </div>
+                @endif
+
+                <div class="store-blog-comments">
+                    @include('Frontend.layouts.comment')
+                </div>
+            </article>
+
+            <div>
+                @if(!empty($article->images['thum']))
+                    <img src="{{ asset($article->images['thum']) }}" alt="{{ $article->title }}" class="store-blog-image">
+                @endif
+            </div>
+        </div>
+    </main>
+@endsection
