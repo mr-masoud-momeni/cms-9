64c62e58456b7526391a0d1a4a4e16b8674cc081
masoud <masoud2525@gmail.com>
2026-09-21 18:14:31 +0330

    new npm run

--- resources/views/Customer/orders/index.blade.php
+++ resources/views/Customer/orders/index.blade.php
@@ -1,138 +1,69 @@
 @extends('Customer.layouts.Master')
 
 @section('content')
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">لیست سفارش‌ها</div>
 
-<div class="row">
+                <div class="panel-body">
+                    @if(session('success'))
+                        <div class="alert alert-success">
+                            {{ session('success') }}
+                        </div>
+                    @endif
 
-    <div class="col-lg-12">
-
-        <div class="panel panel-default">
-
-            <div class="panel-heading">
-                لیست سفارش‌ها
-            </div>
-
-            <div class="panel-body">
-
-                <div class="table-responsive">
-
-                    <table class="table table-striped">
-
-                        <thead>
-                        <tr>
-                            <th>شماره سفارش</th>
-                            <th>خریدار</th>
-                            <th>گیرنده</th>
-                            <th>موبایل</th>
-                            <th>مبلغ</th>
-                            <th>وضعیت</th>
-                            <th>تاریخ پرداخت</th>
-                            <th width="50px">جزئیات</th>
-                        </tr>
-                        </thead>
-
-                        <tbody>
-
-                        @forelse($orders as $order)
-
+                    <div class="table-responsive">
+                        <table class="table table-striped table-bordered">
+                            <thead>
                             <tr>
-
-                                {{-- شماره سفارش --}}
-                                <td>
-                                    #{{ $order->id }}
-                                </td>
-
-                                {{-- خریدار --}}
-                                <td>
-                                    {{ optional($order->buyer)->name ?? '-' }}
-                                </td>
-
-                                {{-- گیرنده --}}
-                                <td>
-                                    {{ $order->receiver_name ?? '-' }}
-                                </td>
-
-                                {{-- شماره گیرنده --}}
-                                <td>
-                                    {{ $order->receiver_phone ?? '-' }}
-                                </td>
-
-                                {{-- مبلغ --}}
-                                <td>
-                                    {{ number_format($order->total ?? optional($order->payment)->amount ?? 0) }}
-                                </td>
-
-                                {{-- وضعیت پرداخت --}}
-                                <td>
-                                    @switch($order->status)
-
-                                        @case('paid')
-                                            <span class="label label-success">
-                                                پرداخت شده
-                                            </span>
-                                            @break
-
-                                        @case('pending')
-                                            <span class="label label-warning">
-                                                در انتظار پرداخت
-                                            </span>
-                                            @break
-
-                                        @default
-                                            <span class="label label-default">
-                                                {{ $order->status ?: 'نامشخص' }}
-                                            </span>
-
-                                    @endswitch
-                                </td>
-
-                                {{-- تاریخ پرداخت --}}
-                                <td>
-                                    @if($order->paid_at)
-                                        {{ $order->paid_at->format('Y/m/d H:i') }}
-                                    @else
-                                        -
-                                    @endif
-                                </td>
-
-                                {{-- جزئیات --}}
-                                <td>
-                                    <a href="{{ route('shop.orders.show', $order->id) }}">
-                                        <i
-                                            class="fa fa-2x fa-pencil-square-o"
-                                            aria-hidden="true"
-                                        ></i>
-                                    </a>
-                                </td>
-
+                                <th>شماره سفارش</th>
+                                <th>خریدار</th>
+                                <th>مبلغ</th>
+                                <th>وضعیت</th>
+                                <th>تاریخ ثبت</th>
+                                <th width="70">جزئیات</th>
                             </tr>
+                            </thead>
 
-                        @empty
-
-                            <tr>
-                                <td colspan="8" class="text-center">
-                                    سفارشی وجود ندارد.
-                                </td>
-                            </tr>
-
-                        @endforelse
-
-                        </tbody>
-
-                    </table>
-
+                            <tbody>
+                            @forelse($orders as $order)
+                                <tr>
+                                    <td>#{{ $order->id }}</td>
+                                    <td>{{ optional($order->buyer)->name ?? '-' }}</td>
+                                    <td>{{ number_format($order->total ?? 0) }} تومان</td>
+                                    <td>
+                                        @if($order->status === 'paid')
+                                            <span class="label label-success">پرداخت شده</span>
+                                        @elseif($order->status === 'pending')
+                                            <span class="label label-warning">در انتظار پرداخت</span>
+                                        @elseif($order->status === 'shipped')
+                                            <span class="label label-info">ارسال شده</span>
+                                        @elseif($order->status === 'completed')
+                                            <span class="label label-success">انجام شده</span>
+                                        @else
+                                            <span class="label label-default">{{ $order->status ?? 'نامشخص' }}</span>
+                                        @endif
+                                    </td>
+                                    <td>{{ $order->created_at }}</td>
+                                    <td class="text-center">
+                                        <a href="{{ route('shop.orders.show', $order) }}" title="مشاهده سفارش">
+                                            <i class="fa fa-eye fa-lg" aria-hidden="true"></i>
+                                        </a>
+                                    </td>
+                                </tr>
+                            @empty
+                                <tr>
+                                    <td colspan="6" class="text-center">
+                                        سفارشی برای این فروشگاه ثبت نشده است.
+                                    </td>
+                                </tr>
+                            @endforelse
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
-
             </div>
-
-            <div class="panel-footer">
-                تعداد سفارش‌ها: {{ $orders->count() }}
-            </div>
-
         </div>
-
     </div>
-
-</div>
-
 @endsection
