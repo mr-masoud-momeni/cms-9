29b2e48e4205c14ff254274c5e0980a4f55d7ab2
masoud <56384888+mr-masoud-momeni@users.noreply.github.com>
2026-09-21 19:29:47 +0330

    Use configurable public path for uploads

--- app/Http/Controllers/customer/CustomerController.php
+++ app/Http/Controllers/customer/CustomerController.php
@@ -1,24 +1,30 @@
 <?php
 
 namespace App\Http\Controllers\customer;
+
 use App\Http\Controllers\Controller;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Intervention\Image\Facades\Image;
-use Intervention\Image\ImageManager;
 
 class CustomerController extends Controller
 {
     protected function UploadImages($file)
     {
-        if(!$file){
-            $url['images']['original'] ="/upload/images/default/post.png";
-            $url['thum'] =$url['images']['original'];
-        }else{
-            $year = carbon::now()->year;
+        if (!$file) {
+            $url['images']['original'] = "/upload/images/default/post.png";
+            $url['thum'] = $url['images']['original'];
+        } else {
+            $year = Carbon::now()->year;
             $imagePath = "/upload/images/{$year}/";
             $filename = $file->getClientOriginalName();
-            $file = $file->move(public_path($imagePath), $filename);
+            $publicPath = config('upload.public_path');
+
+            if (!is_dir($publicPath . $imagePath)) {
+                mkdir($publicPath . $imagePath, 0755, true);
+            }
+
+            $file = $file->move($publicPath . $imagePath, $filename);
             $sizes = ['300', '600', '800'];
             $url['images'] = $this->resize($file->getRealPath(), $sizes, $filename, $imagePath);
             $url['thum'] = $url['images'][$sizes[0]];
@@ -30,13 +36,17 @@ class CustomerController extends Controller
     private function resize($path, $sizes, $filename, $imagePath)
     {
         $images['original'] = $imagePath . $filename;
+
         foreach ($sizes as $size) {
             $images[$size] = $imagePath . "{$size}_" . $filename;
-            Image::make($path)->resize($size, null, function ($constraint) {
-                $constraint->aspectRatio();
-            })->save(public_path($images[$size]));
 
+            Image::make($path)
+                ->resize($size, null, function ($constraint) {
+                    $constraint->aspectRatio();
+                })
+                ->save(config('upload.public_path') . $images[$size]);
         }
+
         return $images;
     }
 }
