1dccd3713b5f0630f2dbdf76e9d67d34b71d503e
masoud <56384888+mr-masoud-momeni@users.noreply.github.com>
2026-09-21 17:57:47 +0330

    Remove unused Pusher and Echo client

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,16 +1,4 @@
-import Echo from 'laravel-echo';
-import Pusher from 'pusher-js';
-
-window.Pusher = Pusher;
-window.Echo = new Echo({
-    broadcaster: 'pusher',
-    key: 'process.env.MIX_PUSHER_APP_KEY',
-    wsHost: window.location.hostname,
-    wsPort: 6001,
-    forceTLS: false,
-    disablestats: true,
-    cluster: 'APP_CLUSTER',
-});
+import axios from 'axios';
 
 window._ = require('lodash');
 
@@ -19,33 +7,5 @@ window._ = require('lodash');
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
-
-window.axios = require('axios');
-
+window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
-
-
-
-
-
-// import Echo from 'laravel-echo';
-// import Pusher from 'pusher-js';
-//
-// window.Pusher = Pusher;
-//
-// window.Echo = new Echo({
-//         broadcaster: 'pusher',
-//         key: import.meta.env.VITE_PUSHER_APP_KEY,
-//     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-//     forceTLS: true
-// });
-
-
-
-
