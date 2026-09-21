97c2a5b93de770cbcd7cbcd09570bae2185a0414
masoud <56384888+mr-masoud-momeni@users.noreply.github.com>
2026-09-21 22:16:55 +0330

    Render article body as plain text with line breaks

--- resources/views/Frontend/Store/Pages/article.blade.php
+++ resources/views/Frontend/Store/Pages/article.blade.php
@@ -16,9 +16,7 @@
             <article class="store-detail-content">
                 <h1>{{ $article->title }}</h1>
 
-                <div class="store-blog-content">
-                    {!! $article->body !!}
-                </div>
+                <div class="store-blog-content">{!! nl2br(e($article->body)) !!}</div>
 
                 @if(isset($article->category))
                     <div class="store-blog-meta">
