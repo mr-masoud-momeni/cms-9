e549cc59b6041f5492adf45711bfff47f6c1e637
masoud <56384888+mr-masoud-momeni@users.noreply.github.com>
2026-09-22 20:59:57 +0330

    Remove legacy storefront index action

--- app/Http/Controllers/front/IndexController.php
+++ app/Http/Controllers/front/IndexController.php
@@ -5,26 +5,14 @@ namespace App\Http\Controllers\front;
 use App\Models\Article;
 use App\Http\Controllers\Controller;
 use App\Models\Product;
-use Illuminate\Http\Request;
 use App\Helpers\ShopHelper;
-use function view;
 
 class IndexController extends Controller
 {
-    public function index()
-    {
-        $shop = ShopHelper::getShop();
-        $shopId = $shop?->id;
-        $articles = Article::where('shop_id', $shopId)->latest()->take(3)->get();
-        $products = Product::where('shop_id', $shopId)->latest()->take(3)->get();
-        return view('Frontend.Home.index', compact('articles', 'products'));
-    }
-
     public function shop()
     {
         $shop = ShopHelper::getShop();
         $shopId = $shop?->id;
-        $menu = Menu::where('id', 1)->first();
 
         $products = Product::where('shop_id', $shopId)
             ->latest()
@@ -42,7 +30,6 @@ class IndexController extends Controller
             'products',
             'articles',
             'shop',
-            'menu',
             'productCount',
             'postCount'
         ));
@@ -54,15 +41,6 @@ class IndexController extends Controller
 
         abort_unless($product->shop_id === $shop->id, 404);
 
-        $menu = Menu::where('id', 1)->first();
-
-        return view('Frontend.Store.show', compact('product', 'menu'));
+        return view('Frontend.Store.show', compact('product'));
     }
-
-    public function create() {}
-    public function store(Request $request) {}
-    public function show($id) {}
-    public function edit($id) {}
-    public function update(Request $request, $id) {}
-    public function destroy($id) {}
 }
