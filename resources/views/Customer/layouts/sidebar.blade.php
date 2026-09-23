09cc7b1d7a94aebbd01b2c8cfbace63278e4c3b9
masoud <56384888+mr-masoud-momeni@users.noreply.github.com>
2026-09-23 23:12:50 +0330

    Keep seller sidebar section open on current page

--- resources/views/Customer/layouts/sidebar.blade.php
+++ resources/views/Customer/layouts/sidebar.blade.php
@@ -1,32 +1,34 @@
 <div id="sidebar-wrapper">
     <ul class="sidebar-nav">
         <li>
-            <a href="{{URL::to('/')}}" target="_blank">نمایش سایت</a>
+            <a href="{{ URL::to('/') }}" target="_blank">نمایش سایت</a>
         </li>
 
         @role('shop_owner')
-        <li class="dropdown ">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">فروشگاه<span class="caret"></span></a>
+        @php
+            $shopMenuOpen = Route::is(
+                'shop.product.*',
+                'shop.gateways.*',
+                'shop.orders.*',
+                'shop.settings.*',
+                'shop.article.*'
+            );
+        @endphp
+
+        <li class="dropdown {{ $shopMenuOpen ? 'sidebar-dropdown-open' : '' }}">
+            <a href="#" class="dropdown-toggle {{ $shopMenuOpen ? 'active-sidebar' : '' }}" data-toggle="dropdown">
+                فروشگاه<span class="caret"></span>
+            </a>
             <ul class="dropdown-menu" role="menu">
-                <li><a class="{{Route::currentRouteName() == 'shop.product.create' ? 'active-sidebar' : ''}}" href="{{route('shop.product.create')}}">ایجاد محصول</a></li>
-                <li><a class="{{Route::currentRouteName() == 'shop.product.index' ? 'active-sidebar' : ''}}" href="{{route('shop.product.index')}}">لیست محصولات</a></li>
-                <li><a class="{{Route::currentRouteName() == 'shop.gateways.edit' ? 'active-sidebar' : ''}}" href="{{route('shop.gateways.edit')}}">مدیریت درگاه پرداخت</a></li>
-                <li><a class="{{Route::currentRouteName() == 'shop.orders.index' ? 'active-sidebar' : ''}}" href="{{route('shop.orders.index')}}">سفارش ها</a></li>
-                <li><a class="{{Route::currentRouteName() == 'shop.settings.edit' ? 'active-sidebar' : ''}}" href="{{route('shop.settings.edit')}}">مشخصات فروشگاه</a></li>
-                <li><a class="{{Route::currentRouteName() == 'shop.article.create' ? 'active-sidebar' : ''}}" href="{{route('shop.article.create')}}">ایجاد مقاله</a></li>
-                <li><a class="{{Route::currentRouteName() == 'shop.article.index' ? 'active-sidebar' : ''}}" href="{{route('shop.article.index')}}">لیست مقالات</a></li>
+                <li><a class="{{ Route::currentRouteName() == 'shop.product.create' ? 'active-sidebar' : '' }}" href="{{ route('shop.product.create') }}">ایجاد محصول</a></li>
+                <li><a class="{{ Route::currentRouteName() == 'shop.product.index' ? 'active-sidebar' : '' }}" href="{{ route('shop.product.index') }}">لیست محصولات</a></li>
+                <li><a class="{{ Route::currentRouteName() == 'shop.gateways.edit' ? 'active-sidebar' : '' }}" href="{{ route('shop.gateways.edit') }}">مدیریت درگاه پرداخت</a></li>
+                <li><a class="{{ Route::currentRouteName() == 'shop.orders.index' ? 'active-sidebar' : '' }}" href="{{ route('shop.orders.index') }}">سفارش ها</a></li>
+                <li><a class="{{ Route::currentRouteName() == 'shop.settings.edit' ? 'active-sidebar' : '' }}" href="{{ route('shop.settings.edit') }}">مشخصات فروشگاه</a></li>
+                <li><a class="{{ Route::currentRouteName() == 'shop.article.create' ? 'active-sidebar' : '' }}" href="{{ route('shop.article.create') }}">ایجاد مقاله</a></li>
+                <li><a class="{{ Route::currentRouteName() == 'shop.article.index' ? 'active-sidebar' : '' }}" href="{{ route('shop.article.index') }}">لیست مقالات</a></li>
             </ul>
         </li>
         @endrole
     </ul>
 </div>
-<script>
-    document.addEventListener('DOMContentLoaded', function () {
-        document.querySelectorAll('.dropdown-menu .active-sidebar').forEach(function (activeLink) {
-            let dropdownParent = activeLink.closest('.dropdown');
-            if (dropdownParent) {
-                dropdownParent.classList.add('open');
-            }
-        });
-    });
-</script>
