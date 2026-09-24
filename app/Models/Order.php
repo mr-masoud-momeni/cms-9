98fc8376f6244c82b50b63e5c74930efeadb514c
masoud <56384888+mr-masoud-momeni@users.noreply.github.com>
2026-09-24 22:39:28 +0330

    Track Bale notification status on orders

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -26,6 +26,7 @@ class Order extends Model
         'paid_at' => 'datetime',
         'reserved_at' => 'datetime',
         'reservation_expires_at' => 'datetime',
+        'bale_notification_sent' => 'boolean',
     ];
 
     public function isReservationExpired(): bool
@@ -60,6 +61,7 @@ class Order extends Model
         'reserved_at',
         'reservation_expires_at',
         'tracking_code',
+        'bale_notification_sent',
         'receiver_name',
         'receiver_phone',
         'receiver_province',
