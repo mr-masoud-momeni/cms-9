a02ffea255b6d6938956194186c3e660d0bd1b9d
masoud <56384888+mr-masoud-momeni@users.noreply.github.com>
2026-09-24 12:50:39 +0330

    Fix Shop type import in PaymentController

--- app/Http/Controllers/front/PaymentController.php
+++ app/Http/Controllers/front/PaymentController.php
@@ -8,6 +8,7 @@ use App\Models\Order;
 use App\Models\Payment;
 use App\Models\PaymentReceipt;
 use App\Models\Product;
+use App\Models\Shop;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Services\OrderReservationService;
