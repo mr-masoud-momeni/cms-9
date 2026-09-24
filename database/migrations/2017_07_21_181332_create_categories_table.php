5ac3ff04f28c4a3b733eb4f81f696b9f6c71d9e8
masoud <56384888+mr-masoud-momeni@users.noreply.github.com>
2026-09-24 14:00:58 +0330

    Add shop_id indexes to table definitions and production SQL

--- database/migrations/2017_07_21_181332_create_categories_table.php
+++ database/migrations/2017_07_21_181332_create_categories_table.php
@@ -23,23 +23,12 @@ class CreateCategoriesTable extends Migration
             $table->string('slug');
             $table->timestamps();
 
+            $table->index('shop_id');
+
             //Define foreign keys
             $table->foreign('shop_id')->references('id')->on('shops')->onDelete('cascade');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
-//        Schema::create('article_category', function (Blueprint $table) {
-//            $table->integer('category_id')->unsigned();
-//            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
-//            $table->integer('article_id')->unsigned();
-//            $table->foreign('article_id')->references('id')->on('articles')->onDelete('cascade');
-//            $table->primary(['article_id' , 'category_id']);
-//        });
-//        Schema::create('categorizables', function (Blueprint $table) {
-//            $table->integer('category_id')->unsigned();
-////            $table->foreign('category_id')->references('id')->on('categories')->onUpdate('cascade')->onDelete('cascade');
-//            $table->integer('categorizable_id')->unsigned();
-//            $table->string('categorizable_type');
-//        });
     }
 
     /**
