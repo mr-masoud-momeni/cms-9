79de3c17e7cbf1a68f0102fb91c25215e022eeff
masoud <56384888+mr-masoud-momeni@users.noreply.github.com>
2026-09-25 14:56:34 +0330

    Add phone to users table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('path')->nullable();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone', 20)->nullable()->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
